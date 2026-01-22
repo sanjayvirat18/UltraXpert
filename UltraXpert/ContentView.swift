3834362bef4e3bd1d129c6893ab6110bdfab6988
Sanjay <sanjayvirat1807@gmail.com>
2026-01-22 12:34:00 +0530

    Initial Commit

---
+++ UltraXpert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UltraXpert
+//
+//  Created by sanjaysadha on 22/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UltraXpert/UltraXpertApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltraXpertApp.swift
+//  UltraXpert
+//
+//  Created by sanjaysadha on 22/01/26.
+//
+
+import SwiftUI
+
+@main
+struct UltraXpertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
